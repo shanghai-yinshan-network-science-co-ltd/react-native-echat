0228d4ddc87a54ce13b98f9e0bc5c9963f4ecfbb
ShineZ <353719503@qq.com>
2022-03-01 18:22:01 +0800

    修改module

--- example/ios/File.swift
+++
@@ -1,6 +0,0 @@
-//
-//  File.swift
-//  YsEchatExample
-//
-
-import Foundation
--- ios/YsEchat.m
+++ ios/YsEchat.m
@@ -1,19 +1,32 @@
 #import "YsEchat.h"
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+#import<EchatVisitorPort/EchatVisitorPort.h>
 
 @implementation YsEchat
 
 RCT_EXPORT_MODULE()
 
-// Example method
-// See // https://reactnative.dev/docs/native-modules-ios
-RCT_REMAP_METHOD(multiply,
-                 multiplyWithA:(nonnull NSNumber*)a withB:(nonnull NSNumber*)b
-                 withResolver:(RCTPromiseResolveBlock)resolve
-                 withRejecter:(RCTPromiseRejectBlock)reject)
-{
-  NSNumber *result = @([a floatValue] * [b floatValue]);
 
-  resolve(result);
+RCT_EXPORT_METHOD(openEChatActivity:(NSString *)uid location:(NSString *)metaData)
+{
+  dispatch_sync(dispatch_get_main_queue(), ^{
+    //创建对话页参数对象
+    Echat_accessConditions * condition = [[Echat_accessConditions alloc] init];
+    //会员身份信息
+    condition.metaData = metaData;
+    //自定义业务参数
+    condition.uid = uid;
+    //打开对话页参数
+    condition.lan = @"id";
+    //打开对话页
+
+    EchatViewController * vc = [[EchatViewController alloc]initWithAccess:condition];
+    EchatNavController *nav = [[EchatNavController alloc] initWithRootViewController:vc];
+    vc.modalPresentationStyle = UIModalPresentationFullScreen;
+    AppDelegate *delegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+    [delegate.window.rootViewController presentViewController:nav animated:YES completion:nil];
+  });
 }
 
 @end
